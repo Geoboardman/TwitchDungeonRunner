b0b828b59734e465ff1c62c761497bb77a9b8664
Geoboardman <saville.rich@gmail.com>
2019-06-14 19:37:27 -0700

    fixed slime movement sync

--- enemy/slime.gd
+++ enemy/slime.gd
@@ -3,15 +3,24 @@ extends "res://engine/entity.gd"
 
 var move_timer_length = 15
 var move_timer = 0
+puppet var puppet_pos = Vector2()
+puppet var puppet_motion = Vector2()
 
 func _ready():
 	SPEED = 40
-	move_dir = dir.rand()
+	if is_network_master():
+		move_dir = dir.rand()
 
 func _physics_process(delta):
+	if is_network_master():
+		if move_timer > 0:
+			move_timer -= 1
+		elif move_timer == 0 or is_on_wall():
+			move_dir = dir.rand()
+			move_timer = move_timer_length
+		rset("puppet_move_dir", move_dir)
+		rset("puppet_pos", position)
+	else:
+		position = puppet_pos
+		move_dir = puppet_motion
 	movement_loop()
-	if move_timer > 0:
-		move_timer -= 1
-	elif move_timer == 0 or is_on_wall():
-		move_dir = dir.rand()
-		move_timer = move_timer_length
